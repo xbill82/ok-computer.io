6b31d6da8bb97f1a6d7e6525ba0c14f8367359d9
Luca Marchesini <xbill82@gmail.com>
2024-12-31 17:57:01 +0100

    Update .gitignore and nuxt.config.ts: Add .output and .DS_Store to .gitignore; include WOFF and WOFF2 assets in Vite configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@/assets/main.css", "primeflex/primeflex.css"],
+  vite: {
+    assetsInclude: ["**/*.woff", "**/*.woff2"],
+  },
 });
